3f73a603ad18bec690a7c7ca4451a03726ce5035
reginakasteen <55086415+reginakasteen@users.noreply.github.com>
2025-03-19 13:50:14 +0200

    Update App.js

--- lab1-test-app/src/App.js
+++ lab1-test-app/src/App.js
@@ -5,6 +5,8 @@ function App() {
 
   return (
     <div className="App">
+    console.log("Hello from remote");
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
